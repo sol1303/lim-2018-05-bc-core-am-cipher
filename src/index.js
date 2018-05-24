434516a5960271f35ae54530f43ae8b64f335624
Sol Lavarello <lavarellosolange@gmail.com>
2018-05-24 18:24:18 -0500

    Avanzes de css, correciones de código

--- src/index.js
+++ src/index.js
@@ -10,45 +10,45 @@ code.addEventListener("click", function(){
     code.style.display= "none";
     decode.style.display= "none";
 codeButton.addEventListener("click", function(){
-var cipherMessage = document.getElementById("message").value.toLowerCase();;
+var cipherMessage = document.getElementById("message").value.toLowerCase();
 var space = document.getElementById("number").value;
-var cipherAnswer = document.getElementById("answer");
 var output = "";
 var cipherNumber = parseInt(space);
     for (var i = 0; i < cipherMessage.length; i++) {
         debugger
          numberLetter = (cipherMessage.charCodeAt(i) + cipherNumber)
-    if (numberLetter <= 90 && numberLetter >= 65 || numberLetter <= 122 && numberLetter >= 97 || numberLetter == 33) {
+    if (numberLetter <= 122 && numberLetter >= 97 || numberLetter == 33) {
             output += String.fromCharCode(numberLetter);
         }
     else {
-            numberLetter2 = (((numberLetter - 97)%26)+97)
-              output += String.fromCharCode(numberLetter2)
+            numberLetterTwo = (((numberLetter - 97)%26)+97)
+              output += String.fromCharCode(numberLetterTwo)
           }
     }
         document.getElementById("answer").innerHTML= space + " - " + output;
 })});
 
 decode.addEventListener("click", function(){
-    //debugger
+    debugger
     showTwo.style.display= "";
     code.style.display= "none";
     decode.style.display= "none";
 decodeButton.addEventListener("click", function(){
-    var cipherMessageTwo = document.getElementById("messageTwo").value.toLowerCase();;
+    debugger
+    var cipherMessageTwo = document.getElementById("messageTwo").value.toLowerCase();
     var spaceTwo = document.getElementById("numberTwo").value;
     var cipherAnswerTwo = document.getElementById("answerTwo");
     var outputTwo = "";
     var cipherNumberTwo = parseInt(spaceTwo);
         for (var i = 0; i < cipherMessageTwo.length; i++) {
-             numberLetterTwo = (cipherMessageTwo.charCodeAt(i)-cipherNumberTwo)
-        if (numberLetterTwo <= 90 && numberLetterTwo >= 65 || numberLetterTwo <= 122 && numberLetterTwo >= 97 || numberLetterTwo == 32) {
-                outputTwo += String.fromCharCode(numberLetterTwo);
+           var numberLetterThree = (cipherMessageTwo.charCodeAt(i)-cipherNumberTwo)
+        if (numberLetterThree <= 122 && numberLetterThree >= 97 || numberLetterThree == 32) {
+                outputTwo += String.fromCharCode(numberLetterThree);
             } 
         else {
-                numberLetterThree = (((numberLetter - 65)%26)+65)
-                  output += String.fromCharCode(numberLetterThree)
+               var numberLetterFour = (((numberLetterThree - 97)%26)+97)
+                  outputTwo += String.fromCharCode(numberLetterFour)
               }
             }
-            document.getElementById("answerTwo").innerHTML= spaceTwo + " - " + outputTwo;
+            document.getElementById("answerTwo").innerHTML= outputTwo;
 })});
