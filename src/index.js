d3422dc8e8da2f8789c9f2be1d2e3fa43df92a9c
Sol Lavarello <lavarellosolange@gmail.com>
2018-05-23 22:50:46 -0500

    Html, botones listos.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,54 @@
-var button = document.getElementById("ButtonCode")
+var code = document.getElementById("start")
+var decode = document.getElementById("startTwo")
+var show = document.getElementById("instructions")
+var showTwo = document.getElementById("instructionsTwo")
+var codeButton = document.getElementById("ButtonCode")
+var decodeButton = document.getElementById("ButtonDecode")
 
+code.addEventListener("click", function(){
+    show.style.display= "";
+    code.style.display= "none";
+    decode.style.display= "none";
+codeButton.addEventListener("click", function(){
+var cipherMessage = document.getElementById("message").value.toLowerCase();;
+var space = document.getElementById("number").value;
+var cipherAnswer = document.getElementById("answer");
+var output = "";
+var cipherNumber = parseInt(space);
+    for (var i = 0; i < cipherMessage.length; i++) {
+        debugger
+         numberLetter = (cipherMessage.charCodeAt(i) + cipherNumber)
+    if (numberLetter <= 90 && numberLetter >= 65 || numberLetter <= 122 && numberLetter >= 97 || numberLetter == 33) {
+            output += String.fromCharCode(numberLetter);
+        }
+    else {
+            numberLetter2 = (((numberLetter - 97)%26)+97)
+              output += String.fromCharCode(numberLetter2)
+          }
+    }
+        document.getElementById("answer").innerHTML= space + " - " + output;
+})});
 
-button.addEventListener("click", function(){
-    var Space = document.getElementById("number")
-    var CipherSpace = Space.value
-    var CipherMessage = document.getElementById("message")
-    var CipherCaesar = CipherMessage.value
-    document.getElementById("answer").innerHTML = CipherSpace + " - " + CipherCaesar
-});
-
-var button = document.getElementById("ButtonDecode")
-
-
-button.addEventListener("click", function(){
-    var Space = document.getElementById("number")
-    var CipherSpace = Space.value
-    var CipherMessage = document.getElementById("message")
-    var CipherCaesar = CipherMessage.value
-    document.getElementById("answer").innerHTML = CipherSpace + " - " + CipherCaesar
-});
+decode.addEventListener("click", function(){
+    //debugger
+    showTwo.style.display= "";
+    code.style.display= "none";
+    decode.style.display= "none";
+decodeButton.addEventListener("click", function(){
+    var cipherMessageTwo = document.getElementById("messageTwo").value.toLowerCase();;
+    var spaceTwo = document.getElementById("numberTwo").value;
+    var cipherAnswerTwo = document.getElementById("answerTwo");
+    var outputTwo = "";
+    var cipherNumberTwo = parseInt(spaceTwo);
+        for (var i = 0; i < cipherMessageTwo.length; i++) {
+             numberLetterTwo = (cipherMessageTwo.charCodeAt(i)-cipherNumberTwo)
+        if (numberLetterTwo <= 90 && numberLetterTwo >= 65 || numberLetterTwo <= 122 && numberLetterTwo >= 97 || numberLetterTwo == 32) {
+                outputTwo += String.fromCharCode(numberLetterTwo);
+            } 
+        else {
+                numberLetterThree = (((numberLetter - 65)%26)+65)
+                  output += String.fromCharCode(numberLetterThree)
+              }
+            }
+            document.getElementById("answerTwo").innerHTML= spaceTwo + " - " + outputTwo;
+})});
