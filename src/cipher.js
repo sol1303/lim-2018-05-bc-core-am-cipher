b17a5cce0082afda7b2d7dc76b076418c2285c18
Sol Lavarello <lavarellosolange@gmail.com>
2018-05-30 11:11:18 -0500

    correciones

--- src/cipher.js
+++ src/cipher.js
@@ -11,8 +11,12 @@ const offset = parseInt(space);
         let ascii = string.charCodeAt(i);
         if(ascii >= 97 && ascii <= 122) {
             output += String.fromCharCode((ascii - 97 + offset) % 26 + 97);
-        } else if(ascii >= 65 && ascii <= 90) {
+        } 
+        else if(ascii >= 65 && ascii <= 90) {
             output += String.fromCharCode((ascii - 65 + offset) % 26 + 65);
+        }
+        else {
+          output += String.fromCharCode(ascii)
         }
     }
         document.getElementById('answer').innerHTML= offset + ' - ' + output;
@@ -29,12 +33,15 @@ decodeButton.addEventListener('click', function(){
     let ascii = string.charCodeAt(i);
     if(ascii >= 97 && ascii <= 122) {
         output += String.fromCharCode((ascii - 97 - offset + 26) % 26 + 97);
-    } else if(ascii >= 65 && ascii <= 90) {
+    } 
+    else if(ascii >= 65 && ascii <= 90) {
         output += String.fromCharCode((ascii - 65 - offset + 26) % 26 + 65);
     } 
+    else {
+      output += String.fromCharCode(ascii)
     document.getElementById('answerTwo').innerHTML= output;
 }
-})
+}})
 
 window.cipher = {
 
