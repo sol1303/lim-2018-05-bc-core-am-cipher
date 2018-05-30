48e39677a6540285c77e48397bfa66453b22cecf
Sol Lavarello <lavarellosolange@gmail.com>
2018-05-30 10:49:19 -0500

    guardando

--- src/cipher.js
+++ src/cipher.js
@@ -4,19 +4,21 @@ const decodeButton = document.getElementById('ButtonDecode')
 codeButton.addEventListener('click', function(){
   debugger
 const string = document.getElementById('message').value;
-const space = document.getElementById('number').value;
+const offset = document.getElementById('number').value;
 let output = '';
-const offset = parseInt(space);
   for(var i = 0; i < string.length; i++) {
         let ascii = string.charCodeAt(i);
         if(ascii >= 97 && ascii <= 122) {
             output += String.fromCharCode((ascii - 97 + offset) % 26 + 97);
-        } else if(ascii >= 65 && ascii <= 90) {
+        } 
+        else if(ascii >= 65 && ascii <= 90) {
             output += String.fromCharCode((ascii - 65 + offset) % 26 + 65);
         }
-    }
+        else {
+          output += String.fromCharCode(ascii);
+        }
         document.getElementById('answer').innerHTML= offset + ' - ' + output;
-    });
+    }});
 
 
 decodeButton.addEventListener('click', function(){
@@ -29,9 +31,13 @@ decodeButton.addEventListener('click', function(){
     let ascii = string.charCodeAt(i);
     if(ascii >= 97 && ascii <= 122) {
         output += String.fromCharCode((ascii - 97 - offset + 26) % 26 + 97);
-    } else if(ascii >= 65 && ascii <= 90) {
+    } 
+    else if(ascii >= 65 && ascii <= 90) {
         output += String.fromCharCode((ascii - 65 - offset + 26) % 26 + 65);
     } 
+    else {
+      output += String.fromCharCode(ascii);
+    }
     document.getElementById('answerTwo').innerHTML= output;
 }
 })
