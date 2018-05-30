58e8678095c5eaeb3b0242b751d6659dc549729d
Sol Lavarello <lavarellosolange@gmail.com>
2018-05-30 09:21:14 -0500

    separación

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,41 @@
+const codeButton = document.getElementById('ButtonCode')
+const decodeButton = document.getElementById('ButtonDecode')
+
+codeButton.addEventListener('click', function(){
+  debugger
+const string = document.getElementById('message').value;
+const space = document.getElementById('number').value;
+let output = '';
+const offset = parseInt(space);
+  for(var i = 0; i < string.length; i++) {
+        let ascii = string.charCodeAt(i);
+        if(ascii >= 97 && ascii <= 122) {
+            output += String.fromCharCode((ascii - 97 + offset) % 26 + 97);
+        } else if(ascii >= 65 && ascii <= 90) {
+            output += String.fromCharCode((ascii - 65 + offset) % 26 + 65);
+        }
+    }
+        document.getElementById('answer').innerHTML= offset + ' - ' + output;
+    });
+
+
+decodeButton.addEventListener('click', function(){
+  debugger
+  const string = document.getElementById('messageTwo').value;
+  const space = document.getElementById('numberTwo').value;
+  let output = '';
+  const offset = parseInt(space);
+  for(var i = 0; i < string.length; i++) {
+    let ascii = string.charCodeAt(i);
+    if(ascii >= 97 && ascii <= 122) {
+        output += String.fromCharCode((ascii - 97 - offset + 26) % 26 + 97);
+    } else if(ascii >= 65 && ascii <= 90) {
+        output += String.fromCharCode((ascii - 65 - offset + 26) % 26 + 65);
+    } 
+    document.getElementById('answerTwo').innerHTML= output;
+}
+})
 
 window.cipher = {
-  
+
 };
--- src/index.js
+++ src/index.js
@@ -1,51 +1,17 @@
-var code = document.getElementById('start')
-var decode = document.getElementById('startTwo')
-var show = document.getElementById('instructions')
-var showTwo = document.getElementById('instructionsTwo')
-var codeButton = document.getElementById("ButtonCode")
-var decodeButton = document.getElementById("ButtonDecode")
+const code = document.getElementById('start')
+const decode = document.getElementById('startTwo')
+const show = document.getElementById('instructions')
+const showTwo = document.getElementById('instructionsTwo')
 
-code.addEventListener("click", function(){
-  show.style.display= "";
-  code.style.display= "none";
-  decode.style.display= "none";
-});
-codeButton.addEventListener("click", function(){
-  debugger
-var string = document.getElementById("message").value;
-var space = document.getElementById("number").value;
-var output = "";
-var offset = parseInt(space);
-  for(var i = 0; i < string.length; i++) {
-        var ascii = string.charCodeAt(i);
-        if(ascii >= 97 && ascii <= 122) {
-            output += String.fromCharCode((ascii - 97 + offset) % 26 + 97);
-        } else if(ascii >= 65 && ascii <= 90) {
-            output += String.fromCharCode((ascii - 65 + offset) % 26 + 65);
-        }
-    }
-        document.getElementById("answer").innerHTML= offset + " - " + output;
-    });
 
-decode.addEventListener("click", function(){
-    debugger
-    showTwo.style.display= "";
-    code.style.display= "none";
-    decode.style.display= "none";
+code.addEventListener('click', function(){
+    show.style.display= '';
+    code.style.display= 'none';
+    decode.style.display= 'none';
+  });
+
+decode.addEventListener('click', function(){
+    showTwo.style.display= '';
+    code.style.display= 'none';
+    decode.style.display= 'none';
 });
-decodeButton.addEventListener("click", function(){
-  debugger
-  var string = document.getElementById("messageTwo").value;
-  var space = document.getElementById("numberTwo").value;
-  var output = "";
-  var offset = parseInt(space);
-  for(var i = 0; i < string.length; i++) {
-    var ascii = string.charCodeAt(i);
-    if(ascii >= 97 && ascii <= 122) {
-        output += String.fromCharCode((ascii - 97 - offset + 26) % 26 + 97);
-    } else if(ascii >= 65 && ascii <= 90) {
-        output += String.fromCharCode((ascii - 65 - offset + 26) % 26 + 65);
-    } 
-    document.getElementById("answerTwo").innerHTML= output;
-}
-})
