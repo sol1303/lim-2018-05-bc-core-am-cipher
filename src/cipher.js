f73c8769482820377d5644cd3f9da77e40d7ebb6
Sol Lavarello <lavarellosolange@gmail.com>
2018-06-04 09:36:55 -0500

    minusculas y signos

--- src/cipher.js
+++ src/cipher.js
@@ -5,7 +5,10 @@ window.cipher = {
       let ascii = string.charCodeAt(i);
       if(ascii >= 65 && ascii <= 90) {
         output += String.fromCharCode(((ascii - 65 + offset)%26) + 65);
-      } else {
+      } else if(ascii >= 97 && ascii <= 122) {
+        output += String.fromCharCode(((ascii - 97 + offset)%26) + 97);
+      }
+      else {
         output += String.fromCharCode(ascii)
       }
   }
@@ -22,9 +25,17 @@ window.cipher = {
       }
         else {
         output += String.fromCharCode(ascii - offset % 26);
-    }
+    } 
+  } else if(ascii >= 97 && ascii <= 122) {
+    if(ascii - offset < 97 && ascii - offset % 26 < 97) {
+    output += String.fromCharCode(123- (97 - (ascii - offset%26 )));
   }
+    else {
+    output += String.fromCharCode(ascii - offset % 26);
+}
+  } else {
+    output += String.fromCharCode(ascii)
   }
   return output;
-},
 }
+}}
